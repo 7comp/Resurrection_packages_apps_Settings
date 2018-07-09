DirtyUnicorns/android_packages_apps_Settings

Add support for overriding vendor properties
Some properties will forcefully be read from vendor instead of system
which can result in outdated or misrepresented OEM information. As a
small workaround, allow overriding the current props that are displayed
in about phone.

This way we can set our own props and display proper information such as
the security patch level. Although this is minor, asthetics matter.
Current supported props are

"ro.vendor.override.security_patch"
"ro.vendor.override.build_display"

To override the device model, simply add this string overlay with the
desired model "config_overridenVendorProductModel"

This is a last option incase we can't override these traditionally

**DU Edits**
- Rework the way we approach this problem. I think in the original commit
there was too much unnecessary things going on. All we need is to override
the security patch level due to Honor being stupid.

- Adapted to our AOSP security dialog

Honor folks just add this prop in your device tree

PRODUCT_PROPERTY_OVERRIDES += \
    ro.vendor.override.security_patch=$(PLATFORM_SECURITY_PATCH)

Change-Id: Ic56a537c38c982ca43b92659ac88d1f2fcdd7fed

src/com/android/settings/deviceinfo/SecurityDialogPreferenceController.java
@@ -18,21 +18,31 @@ package com.android.settings.deviceinfo;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.os.SystemProperties;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 
 import com.android.settings.R;
+import com.android.settings.Utils;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settings.deviceinfo.SecurityDialogFragment;
 import com.android.settingslib.DeviceInfoUtils;
 import com.android.settingslib.core.AbstractPreferenceController;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class SecurityDialogPreferenceController extends AbstractPreferenceController implements
         PreferenceControllerMixin {
 
     private static final String TAG = "SecurityDialogTag";
     private static final String KEY_SECURITY_DIALOG = "security_dialog";
+
+    public static String mHonorPatch;
 
     private final Fragment mHost;
 
@@ -55,8 +65,10 @@ public class SecurityDialogPreferenceController extends AbstractPreferenceContro
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         final Preference pref = screen.findPreference(KEY_SECURITY_DIALOG);
+        mHonorPatch = SystemProperties.get("ro.vendor.override.security_patch", "");
+        String deviceCheck = SystemProperties.get("ro.du.version");
         if (pref == null) return;
-        pref.setSummary(DeviceInfoUtils.getSecurityPatch());
+        pref.setSummary(deviceCheck.contains("berkeley") ? getSecurityPatch() : DeviceInfoUtils.getSecurityPatch());
     }
 
     @Override
@@ -67,5 +79,21 @@ public class SecurityDialogPreferenceController extends AbstractPreferenceContro
             return true;
         }
         return false;
+    }
+
+    public static String getSecurityPatch() {
+        if (!"".equals(mHonorPatch)) {
+            try {
+                SimpleDateFormat template = new SimpleDateFormat("yyyy-MM-dd");
+                Date patchDate = template.parse(mHonorPatch);
+                String format = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dMMMMyyyy");
+                mHonorPatch = DateFormat.format(format, patchDate).toString();
+            } catch (ParseException e) {
+                // broken parse; fall through and use the raw string
+            }
+            return mHonorPatch;
+        } else {
+            return null;
+        }
     }
 }

7comp/Resurrection_packages_apps_Settings

src/com/android/settings/deviceinfo/SecurityDialogPreferenceController.java
@@ -18,21 +18,31 @@ package com.android.settings.deviceinfo;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.os.SystemProperties;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 
 import com.android.settings.R;
+import com.android.settings.Utils;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settings.deviceinfo.SecurityDialogFragment;
 import com.android.settingslib.DeviceInfoUtils;
 import com.android.settingslib.core.AbstractPreferenceController;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class SecurityDialogPreferenceController extends AbstractPreferenceController implements
         PreferenceControllerMixin {
 
     private static final String TAG = "SecurityDialogTag";
     private static final String KEY_SECURITY_DIALOG = "security_dialog";
+
+    public static String mHonorPatch;
 
     private final Fragment mHost;
 
@@ -55,8 +65,10 @@ public class SecurityDialogPreferenceController extends AbstractPreferenceContro
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         final Preference pref = screen.findPreference(KEY_SECURITY_DIALOG);
+        mHonorPatch = SystemProperties.get("ro.vendor.override.security_patch", "");
+        String deviceCheck = SystemProperties.get("ro.rr.version");
         if (pref == null) return;
-        pref.setSummary(DeviceInfoUtils.getSecurityPatch());
+        pref.setSummary(deviceCheck.contains("berkeley") ? getSecurityPatch() : DeviceInfoUtils.getSecurityPatch());
     }
 
     @Override
@@ -67,5 +79,21 @@ public class SecurityDialogPreferenceController extends AbstractPreferenceContro
             return true;
         }
         return false;
+    }
+
+    public static String getSecurityPatch() {
+        if (!"".equals(mHonorPatch)) {
+            try {
+                SimpleDateFormat template = new SimpleDateFormat("yyyy-MM-dd");
+                Date patchDate = template.parse(mHonorPatch);
+                String format = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dMMMMyyyy");
+                mHonorPatch = DateFormat.format(format, patchDate).toString();
+            } catch (ParseException e) {
+                // broken parse; fall through and use the raw string
+            }
+            return mHonorPatch;
+        } else {
+            return null;
+        }
     }
 }